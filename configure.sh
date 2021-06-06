e85edfa9bd6824f8dd1e5248c1d76775e729db37
tanzichao <48847879+tanzichao@users.noreply.github.com>
2021-06-07 04:48:59 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -16,7 +16,7 @@ cat << EOF > /usr/local/etc/v2ray/config.json
 {
     "inbounds": [
         {
-            "port": $PORT,
+            "port": 8080,
             "protocol": "vless",
             "settings": {
                 "clients": [
